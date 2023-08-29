a4a79d9a9af1744dbaeb2365714950205601afbd
Douglas Silva Santos <76598304+skyzzin@users.noreply.github.com>
2023-08-29 03:54:20 -0300

    V1

--- React/src/Components/Projects.jsx
+++ React/src/Components/Projects.jsx
@@ -12,7 +12,7 @@ function Projects() {
     <div className="projects">
         <h1>My Projects</h1>
         <div className="boxDisplay">
-        <a href="https://imoveisgoogle.com.br/" target=''>
+        <a href="https://imoveisgoogle.com.br/" target='_blank'>
         <div className="box">
             <h1>My First Project</h1>
            <img src="mainproject.png" alt="" />
@@ -25,20 +25,19 @@ function Projects() {
         </div>
         </a>
 
-        <a href="https://imoveisgoogle.com.br/" target=''>
+        <a href="https://shop-store-d.netlify.app/" target='_blank' >
         <div className="box">
-            <h1>My First Project</h1>
-           <img src="mainproject.png" alt="" />
+            <h1>My Second Project</h1>
+           <img src="project2.png" alt="" />
             <div className="tags">
               <div className="tag">NodeJS</div>
               <div className="tag">ReactJS</div>
-              <div className="tag">ExpressJS</div>
-              <div className="tag">15-05-2023</div>
+              <div className="tag">29-08-2023</div>
             </div>
         </div>
         </a>
 
-        <a href="https://imoveisgoogle.com.br/" target=''>
+        <a href="https://imoveisgoogle.com.br/" target='_blank'>
         <div className="box">
             <h1>My First Project</h1>
            <img src="mainproject.png" alt="" />
